e28a727444071d9d3b1543a97b4953b04e55dbeb
Neamul Islam Fahim <71332807+NeamulIslamFahim@users.noreply.github.com>
2022-11-16 21:06:48 +0600

    The Ultimate Square

---
+++ The Ultimate Square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n ,k;
+    cin>>n;
+
+    while(n--){
+        cin>>k;
+        cout<<(k+1)/2<<"\n";
+    }
+    return 0;
+}
